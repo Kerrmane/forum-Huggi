839a41efb524c66ebbc9a46ce5de3b5483e128f3
Aymane Kherrati <aymanekherrati88@gmail.com>
2022-04-28 19:15:46 +0200

    premier commit

---
+++ message.php
@@ -0,0 +1,36 @@
+<?php
+// connexion à la base de données 
+require('connexion.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>forum Huggi</title>
+</head>
+<body>
+    <!-- navbar  -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><h3><img src="https://clipartmag.com/images/question-and-answer-images-1.jpg" alt="" width="40" height="34" class="d-inline-block align-text-top">
+      Forum Huggi</h3></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        
+        <a class="nav-link" href="ajouteSujet.php">Publier un sujet</a>
+        <a class="nav-link" href="#"></a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+<!-- navbar  -->
+</body>
+</html>
